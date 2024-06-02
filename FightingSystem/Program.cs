07c4b6229d7b717ac827b9fd5a7468b53d7d5ac0
Vladislav Golosov <v.golosoff@gmail.com>
2024-06-02 15:59:54 +0300

    initial commit

---
+++ FightingSystem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FightingSystem
+{
+     internal class Program
+     {
+          static void Main(string[] args)
+          {
+               
+          }
+     }
+}
+
