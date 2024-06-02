51b2de7ddb5976b10ffc39043d11afcaab4fbfb4
Vladislav Golosov <v.golosoff@gmail.com>
2024-06-02 16:46:00 +0300

    Create class Fighter

--- FightingSystem/Program.cs
+++ FightingSystem/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+﻿
 namespace FightingSystem
 {
      internal class Program
@@ -7,6 +6,37 @@ namespace FightingSystem
           static void Main(string[] args)
           {
                
+          }
+     }
+
+     class Fighter
+     {
+          private string _name;
+          private int _health;
+          private int _damage;
+          private int _armor;
+
+          public Fighter(string name, int health, int damage, int armor)
+          {
+               _name = name;
+               _health = health; 
+               _damage = damage;
+               _armor = armor;
+          }
+
+          public void ShowStats()
+          {
+               Console.WriteLine($"Боец - {_name}, здоровье: {_health}, броня: {_armor}, наносимый урон:{_damage}");
+          }
+
+          public void ShowCurrentHealth()
+          {
+               Console.WriteLine($"{_name} здоровье: {_health}");
+          }
+
+          public void TakeDamage(int damage)
+          {
+               _health -= damage - _armor;
           }
      }
 }
--- FightingSystem/obj/Debug/net8.0/FightingSystem.AssemblyInfo.cs
+++ FightingSystem/obj/Debug/net8.0/FightingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FightingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c4b6229d7b717ac827b9fd5a7468b53d7d5ac0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FightingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FightingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
